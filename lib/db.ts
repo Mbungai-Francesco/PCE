e6b0d432a56f973ef57011278db2390965eb536b
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-11-20 15:48:14 +0100

    prisma issue solved and file issue solved

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+// import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-declare global {
-	var prisma: PrismaClient | undefined;
-}
+const connectionString = `${process.env.DATABASE_URL}`
 
-export const db = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaPg({ connectionString })
+const db = new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== "production") {
-	globalThis.prisma = db;
-}
+export { db }
