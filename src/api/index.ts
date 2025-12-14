a1df3bbbaaed1f8ea249a40fb7ba33cd3491e359
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-12-14 17:38:31 +0100

    backend url uncommented

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
-const port = 5000
-export const link = `http://localhost:${port}`
-// export const link = `https://pce-backend.onrender.com`
+// const port = 5000
+// export const link = `http://localhost:${port}`
+export const link = `https://pce-backend.onrender.com`
